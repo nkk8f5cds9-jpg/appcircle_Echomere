a21cb4ec910eeab7c1957f78613dfa1ef1ece36a
nkk8f5cds9-jpg <nkk8f5cds9@privaterelay.appleid.com>
2026-02-16 23:19:30 +0200

    Delete Finance Echo Collectot directory

--- Finance Echo Collectot/ContentView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        MainTabView()
-            .environment(\.managedObjectContext, CoreDataStack.shared.viewContext)
-    }
-}
-
